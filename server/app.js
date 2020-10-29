48c7142876ef0721511b778b45a01866502474de
bradeggins <brad.eggins@gmail.com>
2020-10-29 13:31:31 +1300

    Add morgan middleware

--- server/app.js
+++ server/app.js
@@ -4,11 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var morgan_1 = __importDefault(require("morgan"));
 var app = express_1.default();
 app.set("port", process.env.PORT || 3000);
 app.use(express_1.default.json());
-app.get('/', function (req, res) {
-    res.send('Hello World Express + TypeScript + Linting');
+app.use(morgan_1.default(':date[web] :method :url :status :res[content-length] - :response-time ms'));
+app.get("/", function (req, res) {
+    res.status(200).send("Hello World Express + TypeScript + Linting");
 });
 exports.default = app;
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response } from "express";
+import morgan from 'morgan'
 
 const app: express.Application = express();
 
 app.set("port", process.env.PORT || 3000);
 app.use(express.json());
+app.use(morgan(':date[web] :method :url :status :res[content-length] - :response-time ms'))
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World Express + TypeScript + Linting");
+  res.status(200).send("Hello World Express + TypeScript + Linting");
 });
 
 export default app;
